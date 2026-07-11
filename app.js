ba2b754f252620b7d34aae6b319caa6a256b7c97
unknown <varga.andrew.90@gmail.com>
2026-07-11 18:27:28 +0200

    Add smoot scrolling to main page aboute section with text

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const menu = document.querySelector('#mobile-menu'); //so its target the mobile-menu id element (#menas id)
 const menuLinks = document.querySelector('.navbar__menu');
 
+function closeMobileMenu() {
+    menu.classList.remove('is-active');
+    menuLinks.classList.remove('active');
+}
+
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');//check whats mean toggle the classLists
     menuLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.navbar__links').forEach(link => {
+    link.addEventListener('click', closeMobileMenu);
 });
 
 // FAQ accordion behavior
